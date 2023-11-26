b60ee51c1ea373903c3b29738bc4054fee41f24b
Nikos Kechris <nkechris22@gmail.com>
2023-11-26 17:44:37 +0200

    Create timestamps

--- api/handlers/url_handlers.go
+++ api/handlers/url_handlers.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"net/http"
 	"redirectUrls/api/models"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func GetUrls(c *gin.Context) {
-	rows, err := db.Query("SELECT id, currentUrl, redirectUrl FROM urls")
+	rows, err := db.Query("SELECT * FROM urls")
 	if err != nil {
 		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -21,7 +22,7 @@ func GetUrls(c *gin.Context) {
 	var urls []models.URL
 	for rows.Next() {
 		var url models.URL
-		err := rows.Scan(&url.ID, &url.CurrentUrl, &url.RedirectUrl)
+		err := rows.Scan(&url.ID, &url.CurrentUrl, &url.RedirectUrl, &url.CreatedAt, &url.UpdatedAt)
 		if err != nil {
 			c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
@@ -36,8 +37,8 @@ func GetUrlByID(c *gin.Context) {
 	id := c.Param("id")
 
 	var url models.URL
-	err := db.QueryRow("SELECT id, currentUrl, redirectUrl FROM urls WHERE id = ?", id).
-		Scan(&url.ID, &url.CurrentUrl, &url.RedirectUrl)
+	err := db.QueryRow("SELECT * FROM urls WHERE id = ?", id).
+		Scan(&url.ID, &url.CurrentUrl, &url.RedirectUrl, &url.CreatedAt, &url.UpdatedAt)
 
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -58,7 +59,6 @@ func CreateUrl(c *gin.Context) {
 		return
 	}
 
-	// Check if the currentUrl already exists in the database
 	var existingID string
 	err := db.QueryRow("SELECT id FROM urls WHERE currentUrl = ?", newURL.CurrentUrl).Scan(&existingID)
 	if err == nil {
@@ -69,13 +69,12 @@ func CreateUrl(c *gin.Context) {
 		return
 	}
 
-	result, err := db.Exec("INSERT INTO urls (currentUrl, redirectUrl) VALUES (?, ?)", newURL.CurrentUrl, newURL.RedirectUrl)
+	result, err := db.Exec("INSERT INTO urls (currentUrl, redirectUrl, createdAt, updatedAt) VALUES (?, ?, ?, ?)", newURL.CurrentUrl, newURL.RedirectUrl, time.Now().Unix(), time.Now().Unix())
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError, "error": err.Error()})
 		return
 	}
 
-	// Get the auto-incremented ID of the newly inserted URL
 	newID, err := result.LastInsertId()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError, "error": err.Error()})
@@ -84,7 +83,7 @@ func CreateUrl(c *gin.Context) {
 
 	newURL.ID = fmt.Sprintf("%d", newID)
 
-	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "data": newURL})
+	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Url created successfully"})
 }
 
 func DeleteUrlByID(c *gin.Context) {
@@ -123,8 +122,8 @@ func PatchUrlByID(c *gin.Context) {
 	}
 
 	var existingURL models.URL
-	err = db.QueryRow("SELECT id, currentUrl, redirectUrl FROM urls WHERE id = ?", id).
-		Scan(&existingURL.ID, &existingURL.CurrentUrl, &existingURL.RedirectUrl)
+	err = db.QueryRow("SELECT * FROM urls WHERE id = ?", id).
+		Scan(&existingURL.ID, &existingURL.CurrentUrl, &existingURL.RedirectUrl, &existingURL.CreatedAt, &existingURL.UpdatedAt)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError, "error": err.Error()})
 		return
@@ -134,11 +133,19 @@ func PatchUrlByID(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest, "error": err.Error()})
 		return
 	}
-	_, err = db.Exec("UPDATE urls SET redirectUrl = ? WHERE id = ?", existingURL.RedirectUrl, id)
+
+	_, err = db.Exec("UPDATE urls SET redirectUrl = ?, updatedAt = ? WHERE id = ?", existingURL.RedirectUrl, time.Now().Unix(), id)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError, "error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": existingURL})
+	err = db.QueryRow("SELECT * FROM urls WHERE id = ?", id).
+		Scan(&existingURL.ID, &existingURL.CurrentUrl, &existingURL.RedirectUrl, &existingURL.CreatedAt, &existingURL.UpdatedAt)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError, "error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Url updated successfully"})
 }
