2022998ff2e260666be551c1763b0f922aa39ebc
Nikos Kechris <nkechris22@gmail.com>
2023-11-26 13:22:28 +0200

    Add mysql to fetch data

--- api/handlers/url_handlers.go
+++ api/handlers/url_handlers.go
@@ -1,29 +1,80 @@
 package handlers
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
+	"os"
+
 	"redirectUrls/api/models"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
-// TODO REPLACE WITH REAL DATA
-var urls = []models.URL{
-	{ID: "1", CurrentUrl: "/blackfriday", RedirectUrl: "/cybermonday"},
-	{ID: "2", CurrentUrl: "/blackfriday", RedirectUrl: "/cybermonday"},
+var db *sql.DB
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(fmt.Errorf("Error loading .env file: %s", err))
+	}
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dbSource := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPassword, dbHost, dbName)
+
+	var openErr error
+	db, openErr = sql.Open("mysql", dbSource)
+	if openErr != nil {
+		panic(openErr.Error())
+	}
+	pingErr := db.Ping()
+	if pingErr != nil {
+		panic(pingErr.Error())
+	}
 }
 
 func GetUrls(c *gin.Context) {
+	rows, err := db.Query("SELECT id, currentUrl, redirectUrl FROM urls")
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var urls []models.URL
+	for rows.Next() {
+		var url models.URL
+		err := rows.Scan(&url.ID, &url.CurrentUrl, &url.RedirectUrl)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		urls = append(urls, url)
+	}
+
 	c.IndentedJSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": urls})
 }
 
 func GetUrlByID(c *gin.Context) {
 	id := c.Param("id")
-	for _, url := range urls {
-		if url.ID == id {
-			c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": url})
-			return
+
+	var url models.URL
+	err := db.QueryRow("SELECT id, currentUrl, redirectUrl FROM urls WHERE id = ?", id).
+		Scan(&url.ID, &url.CurrentUrl, &url.RedirectUrl)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "URL not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError, "error": err.Error()})
 		}
+		return
 	}
-	c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "URL not found"})
+
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": url})
 }
